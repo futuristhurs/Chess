4f468bfc26c7f9e55793675ee10b0a40516e23d2
Egbodor CollinsS <120804886+futuristhurs@users.noreply.github.com>
2024-03-07 22:37:58 +0100

    idk

--- ChessMain.py
+++ ChessMain.py
@@ -1,5 +1,5 @@
 import pygame as p
-from Chess import ChessEngine
+from CHESS import ChessEngine
 
 WIDTH = HEIGHT = 512 #400 IS ANOTHER GOOD OPTION
 DIMENSION = 8
@@ -10,4 +10,52 @@ IMAGES = {}
 def loadImages():
     pieces = ['wp', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bp', 'bR', 'bN', 'bB', 'bQ', 'bK']
     for piece in pieces:
-        IMAGES[piece] = p.image.load(f'./images/{piece}.png').convert_alpha()
+        IMAGES[piece] = p.transform.scale(p.image.load('images/' + piece + '.png'), (SQ_SIZE, SQ_SIZE))
+
+#note: we can access an image by saying 'IMAGES['wp']'
+'''
+The main driver for our code. this will handle user input and updating the graphics
+'''
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.color("white"))
+    gs = ChessEngine.GameState()
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.Quit:
+                running = False
+        
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+
+
+def drawBoard(screen):
+    colors = [p.color("white"), p.color("grey")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r + c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": 
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+if __name__ == "__main__":
+    main()
+            
+     
